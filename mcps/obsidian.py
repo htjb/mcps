86c8e2aa78a63281801a3f018c277cb2633a5db3
Harry Bevins <harrytjbevins@yahoo.co.uk>
2026-03-30 20:38:09 +0100

    ollama -> obsidian

--- mcps/obsidian.py
+++ mcps/obsidian.py
@@ -1,14 +1,12 @@
 import os
 
-import ollama
 from mcp.server.fastmcp import FastMCP
-from pydantic import Field
 
-mcp = FastMCP(name="Ollama MCP")
+mcp = FastMCP(name="Obsidian MCP")
 
 
 def main() -> None:
-    """Run the Ollama MCP server."""
+    """Run the Obsidian MCP server."""
     mcp.run()
 
 if __name__ == "__main__":
